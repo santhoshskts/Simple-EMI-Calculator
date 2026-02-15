d6e2477e3c583b2fe64b834d3b561d00560129e6
Santhosh S <101112325+santhoshskts@users.noreply.github.com>
2026-02-15 22:05:02 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+
+echo "Enter Principal:"
+read principal
+
+echo "Enter Rate of Interest:"
+read rate
+
+echo "Enter Time (in years):"
+read time
+
+simple_interest=$((principal * rate * time / 100))
+
+echo "Simple Interest is: $simple_interest"
 
